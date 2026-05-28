f8b43b47aac82722d95e4b3972ed5daf41c701ef
Afeziwe10 <167782671+Afeziwe10@users.noreply.github.com>
2026-05-28 20:54:08 +0200

    Updated main activity for login validation

--- app/src/main/java/com/example/budgetbuddyapp/MainActivity.kt
+++ app/src/main/java/com/example/budgetbuddyapp/MainActivity.kt
@@ -5,16 +5,50 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.content.Intent
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        auth = FirebaseAuth.getInstance()
+
+        //Calling the components
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<MaterialButton>(R.id.btnLogin)
+        val tvRegister = findViewById<TextView>(R.id.tvRegisterRedirect)
+
+        //Login Button
+        btnLogin.setOnClickListener{
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            //Validation
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }else {
+                //Firebase login
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, DashboardActivity:: class.java ))
+                        finish()
+                    }else{
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+        //Register User
+        tvRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
